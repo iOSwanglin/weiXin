1fa58cace1509172ed3edc726be4edea8e3791a2
iOSwanglin <273656856@qq.com>
2016-09-03 17:50:07 +0800

    测试

---
+++ HHWeiXin/HHWeiXin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHWeiXin
+//
+//  Created by 聚智 on 16/9/3.
+//  Copyright © 2016年 聚智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HHWeiXin/HHWeiXin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HHWeiXin
+//
+//  Created by 聚智 on 16/9/3.
+//  Copyright © 2016年 聚智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
